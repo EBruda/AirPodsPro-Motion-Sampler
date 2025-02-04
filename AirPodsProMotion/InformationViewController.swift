c2bfea69d249f1420b63771da3c9eb4cbfe7e0d7
Elizabeth Bruda <elizabeth.bruda@gatech.edu>
2025-02-04 12:57:08 -0500

    Update InformationViewController.swift

--- AirPodsProMotion/InformationViewController.swift
+++ AirPodsProMotion/InformationViewController.swift
@@ -1,84 +1,61 @@
-//
-//  ViewController.swift
-//  AirPodsProMotion
-//
-//  Created by Yoshio on 2020/09/22.
-//
-
 import UIKit
 import CoreMotion
 
-class InformationViewController: UIViewController, CMHeadphoneMotionManagerDelegate {
+class InformationViewController: UIViewController {
+
+    private let pedometer = CMPedometer()
 
     lazy var textView: UITextView = {
         let view = UITextView()
         view.frame = CGRect(x: self.view.bounds.minX + (self.view.bounds.width / 10),
                             y: self.view.bounds.minY + (self.view.bounds.height / 6),
-                            width: self.view.bounds.width, height: self.view.bounds.height)
+                            width: self.view.bounds.width * 0.8, height: self.view.bounds.height * 0.3)
         view.text = "Looking for AirPods Pro"
-        view.font = view.font?.withSize(14)
+        view.font = UIFont.systemFont(ofSize: 18)
         view.isEditable = false
+        view.textAlignment = .center
         return view
     }()
-    
-    
-    
-    //AirPods Pro => APP :)
-    // let APP = CMHeadphoneMotionManager()
-    let APP_2 = CMPedometer()
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Information View"
         view.backgroundColor = .systemBackground
         view.addSubview(textView)
-        
-        
-        APP_2.delegate = self
-        
-        // guard APP_2.isDeviceMotionAvailable else {
-        //     AlertView.alert(self, "Sorry", "Your device is not supported.")
-        //     textView.text = "Sorry, Your device is not supported."
-        //     return
-        // }
-        
+
+        startPedometerUpdates()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        stopPedometerUpdates()
+    }
+
+    private func startPedometerUpdates() {
         guard CMPedometer.isPaceAvailable() else {
-            print("Pace data is not available on this device.")
+            textView.text = "Pace data is not available on this device."
             return
         }
-        // APP.startDeviceMotionUpdates(to: OperationQueue.current!, withHandler: {[weak self] motion, error  in
-        //     guard let motion = motion, error == nil else { return }
-        //     self?.printData(motion)
-        // })
 
-        
-        APP_2.startUpdates(from: Date()) { (data, error) in
-            guard let data = data, error == nil else {
-                print("Error fetching pedometer data: \(error?.localizedDescription ?? "Unknown error")")
+        pedometer.startUpdates(from: Date()) { [weak self] data, error in
+            guard let self = self, let data = data, error == nil else {
+                DispatchQueue.main.async {
+                    self?.textView.text = "Error fetching pedometer data."
+                }
                 return
             }
 
-            if let pace = data.currentPace {
-                print("Current Pace: \(pace) steps per second")
-                self.textView.text = """
-                    Pace: 
-                        \(data.currentPace)
-                    """
-            } else {
-                print("Pace data is not available yet.")
+            DispatchQueue.main.async {
+                if let pace = data.currentPace {
+                    self.textView.text = "Current Pace: \(pace) steps/sec"
+                } else {
+                    self.textView.text = "Pace data is not available yet."
+                }
             }
         }
+    }
 
+    private func stopPedometerUpdates() {
+        pedometer.stopUpdates()
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        self.viewDidLoad()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        APP.stopDeviceMotionUpdates()
-        APP_2.stopDeviceMotionUpdates()
-    }
-    
 }
